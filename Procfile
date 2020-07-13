b1d76563d5572f499f774fe141c0cd7dc5891e82
Yamila Moreno <yamila.ms@gmail.com>
2020-07-13 18:14:43 +0200

    Ready to deploy in Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn coco_material.wsgi
--- coco_material/settings.py
+++ coco_material/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import django_heroku
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -148,3 +151,5 @@ REST_FRAMEWORK = {
     # ]
 }
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
