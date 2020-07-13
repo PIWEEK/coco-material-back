ca2629214f6ab3ab939d3cc278e3ae4ebd7ab61e
Yamila Moreno <yamila.ms@gmail.com>
2020-07-13 13:08:11 +0200

    Basic admin configuration

--- vectors/admin.py
+++ vectors/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 
 from vectors.models import Vector
 
 
 @admin.register(Vector)
 class VectorAdmin(admin.ModelAdmin):
-    ...
+    fieldsets = (
+        ('Main', {'fields': ('name', 'tags',)}),
+        ('Vector', {'fields': ('svg', 'svg_image')}),
+    )
+    list_display = ['name', 'tag_list']
+    readonly_fields = ['svg_image',]
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).prefetch_related('tags')
+
+    def tag_list(self, obj):
+        return u", ".join(o.name for o in obj.tags.all())
+
+    def svg_image(self, obj):
+        return mark_safe(f'<img src="{obj.svg.url}" width=200 height=200 />')
