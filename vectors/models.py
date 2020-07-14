b3cf1ef7793116a0ee95130cf5b0c2d08dd3fbb2
Yamila Moreno <yamila.ms@gmail.com>
2020-07-14 12:07:15 +0200

    Delete files from filesystem

--- vectors/models.py
+++ vectors/models.py
@@ -1,5 +1,8 @@
-from django.db import models
+import os
 
+from django.core.files.storage import default_storage
+from django.db import models
+from django.dispatch import receiver
 from taggit.managers import TaggableManager
 
 
@@ -10,3 +13,26 @@ class Vector(models.Model):
 
     def __str__(self):
         return self.name
+
+
+@receiver(models.signals.post_delete, sender=Vector)
+def auto_delete_svg_on_deletion(sender, instance, **kwargs):
+    if instance.svg:
+        if os.path.isfile(instance.svg.path):
+            default_storage.delete(instance.svg.path)
+
+
+@receiver(models.signals.pre_save, sender=Vector)
+def auto_delete_file_on_update(sender, instance, **kwargs):
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = Vector.objects.get(pk=instance.pk).svg
+    except Vector.DoesNotExist:
+        return False
+
+    new_file = instance.svg
+    if old_file != new_file:
+        if os.path.isfile(old_file.path):
+            default_storage.delete(old_file.path)
