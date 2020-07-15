b46a2c1a256725cd63b466662d9111b42d9b68bc
Yamila Moreno <yamila.ms@gmail.com>
2020-07-15 15:36:12 +0200

    Download bulk and blockchain

--- vectors/urls.py
+++ vectors/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework import routers as drf_routers
 
 from vectors.viewsets import VectorViewSet, TagViewSet
+from vectors.views import BulkDownload
 
 # Automatic routes
 router = drf_routers.DefaultRouter()
@@ -10,5 +11,6 @@ router.register(r'tags', TagViewSet, basename='tag')
 
 # Other urls
 vector_urls = [
+    path('bulk_download/', BulkDownload.as_view(), name='me'),
     path('', include(router.urls)),
 ]
--- vectors/views.py
+++ vectors/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from os.path import basename
+from uuid import uuid4
+from zipfile import ZipFile
 
-# Create your views here.
+from django.db.models import Q
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from vectors.models import Vector
+
+
+class BulkDownload(APIView):
+    def get(self, request):
+        if 'tags' not in request.query_params:
+            response = Response({'error': 'tags are mandatory'}, status=400)
+            return response
+
+        tags = request.query_params['tags'].split(',')
+        and_condition = Q()
+        for tag in tags:
+            and_condition.add(Q(tags__name=tag), Q.AND)
+
+        vectors = Vector.objects.filter(and_condition).distinct()
+        if not vectors:
+            response = Response({'error': 'there are no vectors with these tags'}, status=400)
+            return response
+
+        zip_name = f'delete/cocomaterial_{uuid4().hex}.zip'
+        with ZipFile(zip_name, 'w') as zipfile:
+            for vector in vectors:
+                zipfile.write(vector.svg.path, basename(vector.svg.name))
+
+        with open(zip_name, 'rb') as f:
+            zip_file = f.read()
+            response = HttpResponse(zip_file, content_type='application/zip')
+            response['Content-Disposition'] = 'attachment; filename="cocomaterial.zip"'
+            return response
