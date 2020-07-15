058ef89b973cbaa1edea35cbf9a54afea16addac
Yamila Moreno <yamila.ms@gmail.com>
2020-07-15 16:19:27 +0200

    Delete temporary zip and slow-frequency trading

--- vectors/views.py
+++ vectors/views.py
@@ -1,3 +1,4 @@
+import os
 from os.path import basename
 from uuid import uuid4
 from zipfile import ZipFile
@@ -26,13 +27,16 @@ class BulkDownload(APIView):
             response = Response({'error': 'there are no vectors with these tags'}, status=400)
             return response
 
-        zip_name = f'delete/cocomaterial_{uuid4().hex}.zip'
+        zip_name = f'cocomaterial_{uuid4().hex}.zip'
         with ZipFile(zip_name, 'w') as zipfile:
             for vector in vectors:
                 zipfile.write(vector.svg.path, basename(vector.svg.name))
 
+        response = None
         with open(zip_name, 'rb') as f:
             zip_file = f.read()
             response = HttpResponse(zip_file, content_type='application/zip')
             response['Content-Disposition'] = 'attachment; filename="cocomaterial.zip"'
-            return response
+
+        os.remove(zip_name)
+        return response
