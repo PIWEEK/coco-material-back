fc2ce36c735b2afe6b60609aaac1d7ae2748a981
Yamila Moreno <yamila.ms@gmail.com>
2020-07-16 17:17:36 +0200

    Minando bitcoins con lambdas y kubernetes

--- vectors/admin.py
+++ vectors/admin.py
@@ -1,11 +1,28 @@
+from django.contrib.admin import widgets
 from django.contrib import admin
+from django import forms
 from django.utils.html import mark_safe
+from taggit.models import Tag
 
 from vectors.models import Vector
 
 
+class VectorForm(forms.ModelForm):
+    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.none())
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['tags'].widget = widgets.FilteredSelectMultiple('Tags', False)
+        self.fields['tags'].queryset = Tag.objects.all()
+
+    class Meta:
+        model = Vector
+        exclude = []
+
+
 @admin.register(Vector)
 class VectorAdmin(admin.ModelAdmin):
+    form = VectorForm
     fieldsets = (
         ('Main', {'fields': ('name', 'tags',)}),
         ('Vector', {'fields': ('svg', 'svg_image')}),
@@ -15,6 +32,7 @@ class VectorAdmin(admin.ModelAdmin):
     list_editable = ['tags']
     list_filter = ['tags']
     search_fields = ['name']
+    save_on_top = True
 
     def get_queryset(self, request):
         return super().get_queryset(request).prefetch_related('tags')
@@ -24,4 +42,3 @@ class VectorAdmin(admin.ModelAdmin):
 
     def svg_image_thumb(self, obj):
         return mark_safe(f'<img src="{obj.svg.url}" width=70 height=70 />')
-
