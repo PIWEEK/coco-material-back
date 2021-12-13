79d8d8377fee9206d49f466b02864d2a7caad5f5
David Barragán Merino <david.barragan@kaleidos.net>
2021-12-13 11:03:48 +0100

    Fix warning mesage

--- coco_material/settings.py
+++ coco_material/settings.py
@@ -138,7 +138,7 @@ TAGGIT_CASE_INSENSITIVE = True
 
 # DRF
 REST_FRAMEWORK = {
-    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
