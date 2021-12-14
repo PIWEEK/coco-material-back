f20f5cc758fc20851b67f7bd072f27f859d866b3
David Barragán Merino <david.barragan@kaleidos.net>
2021-12-14 13:11:13 +0100

    Remove default pagination settings to fix an error

--- coco_material/settings.py
+++ coco_material/settings.py
@@ -139,8 +139,6 @@ TAGGIT_CASE_INSENSITIVE = True
 
 # DRF
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
     'DEFAULT_RENDERER_CLASSES': (
         'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
         'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
